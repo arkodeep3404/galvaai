dab4523055842a88e87ba1b37028250d2068a489
Arkodeep Chatterjee <arkodeep3404@gmail.com>
2024-04-07 22:18:11 +0530

    minor updates

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/backend-api/api/v1", rootRouter);
+app.use("/backend-api/v1", rootRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).json({
